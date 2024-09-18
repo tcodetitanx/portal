f1e70ea03e073bc3560b166b74f3473c2b9a368f
tcodetitanx <codetitanx@gmail.com>
2024-09-18 12:58:39 -0600

    u

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./promnote/styles.css">
     <title>Login</title>
     
 </head>
--- portal.php
+++ portal.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./promnote/styles.css">
     <title>Portal</title>
 </head>
 <body>
