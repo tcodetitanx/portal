a8a9f1246ef07bf5a1c43f110635bcd0e950d26e
tcodetitanx <codetitanx@gmail.com>
2024-10-09 09:39:18 -0600

    u

--- clean/generate_pdf.php
+++ clean/generate_pdf.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('vendor/tecnickcom/tcpdf/tcpdf.php');
+require_once('../vendor/tecnickcom/tcpdf/tcpdf.php');
 
 // Create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
